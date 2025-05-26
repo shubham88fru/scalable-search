0e8f193fdf0e60ff7b1c2b93ad2ad6d4ac48e9ad
shubham88fru <shubham88fru@yahoo.com>
2025-05-26 09:59:19 -0700

    bulk operations

--- java-es-playground/src/test/java/com/learning/javaesplayground/p02/CrudOperationsTest.java
+++ java-es-playground/src/test/java/com/learning/javaesplayground/p02/CrudOperationsTest.java
@@ -8,6 +8,10 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.util.Streamable;
+
+import java.util.List;
+import java.util.stream.IntStream;
 
 public class CrudOperationsTest extends AbstractTest {
     private static final Logger log = LoggerFactory.getLogger(CrudOperationsTest.class);
@@ -37,6 +41,36 @@ public class CrudOperationsTest extends AbstractTest {
         printAll();
 
         Assertions.assertTrue(employeeRepository.findById(1).isEmpty());
+
+    }
+
+    @Test
+    public void bulkCrud() {
+        List<Employee> employeeList =  IntStream.rangeClosed(1, 10)
+                .mapToObj(i -> createEmployee(i, "name-" + i, 20 + i))
+                .toList();
+        employeeRepository.saveAll(employeeList);
+        printAll();
+
+        Assertions.assertEquals(10, employeeRepository.count());
+
+        List<Integer> ids = List.of(2, 4, 6);
+        Iterable<Employee> it = employeeRepository.findAllById(ids);
+        employeeList = Streamable.of(it).toList();
+
+        Assertions.assertEquals(3, employeeList.size());
+
+        employeeList.forEach(e -> e.setAge(e.getAge() + 10));
+        employeeRepository.saveAll(employeeList);
+        printAll();
+
+        employeeRepository.findAllById(ids)
+                .forEach(e -> Assertions.assertEquals(e.getId() + 30, e.getAge()));
+
+        employeeRepository.deleteAllById(ids);
+        printAll();
+
+        Assertions.assertEquals(7, employeeRepository.count());
 
     }
 
