a02cd97a42edd965c2024df98e5a82fd7d518fc7
shubham88fru <shubham88fru@yahoo.com>
2025-05-31 11:15:03 -0700

    initial commit

---
+++ near-business/src/main/java/com/learning/nearbusiness/NearBusinessApplication.java
@@ -0,0 +1,13 @@
+package com.learning.nearbusiness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NearBusinessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NearBusinessApplication.class, args);
+    }
+
+}
